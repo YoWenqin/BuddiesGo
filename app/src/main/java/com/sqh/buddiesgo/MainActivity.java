609b7253c78132c39de9642488be91e65400ca71
Jiamin <jli79@smith.edu>
2017-01-17 12:21:18 -0800

    Updated colors.xml and mainActivity.xml

--- app/src/main/java/com/sqh/buddiesgo/MainActivity.java
+++ app/src/main/java/com/sqh/buddiesgo/MainActivity.java
@@ -2,6 +2,8 @@ package com.sqh.buddiesgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Creates event listeners for home page buttons
+        final Button foodButton = (Button) findViewById(R.id.foodButton);
+        foodButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Perform action on click
+                // Call food buddy page
+            }
+        });
+
+        final Button hikingButton = (Button) findViewById(R.id.hikingButton);
+        hikingButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Call hiking buddy page
+            }
+        });
+
+        final Button movieButton = (Button) findViewById(R.id.movieButton);
+        movieButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Call movie buddy page
+            }
+        });
     }
 }
